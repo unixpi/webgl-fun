118196c3d63be8b94d6e9ac07bc7398ad1ea9c5b
sach <sacha.saint-leger@mail.mcgill.ca>
2016-05-05 10:45:04 -0400

    completed lesson 9

--- webgl-lessons-master/js/lesson09.js
+++ webgl-lessons-master/js/lesson09.js
@@ -202,7 +202,7 @@ function initBuffers() {
     starVertexTextureCoordBuffer.numItems = 4;
 }
 
-
+//the (rather dull) code that draws the star: it just draws a square in a manner that will be familiar from the first lesson, using an appropriate texture and vertex position/texture coordinate buffers
 function drawStar() {
     gl.activeTexture(gl.TEXTURE0);
     gl.bindTexture(gl.TEXTURE_2D, starTexture);
@@ -220,6 +220,15 @@ function drawStar() {
 
 
 
+//JavaScript's object model is very different to other languages'. The way I've found it easiest to understand is that each object is created as a dictionary (or hashtable, or associative array) and then is turned into a fully-fledged object by putting values into it. The object's fields are simply entries in the dictionary that map to values, and the methods are entries that map to functions. Now, we add to that the fact that for single-word keys, the syntax foo.bar is a valid JavaScript shortcut for foo["bar"], and you can see how you get syntax similar to other languages' from a very different starting point.
+
+//Next, when you're in any JavaScript function, there is an implicitly-bound variable called this which refers to the function's "owner". For global functions this is a global per-page "window" object, but if you put the keyword new before it then it will be a brand-new object instead. So, if you have a function that sets this.foo to 1 and this.bar to a function, and then you make sure you always call it with the new keyword, it's basically the same as a constructor combined with a class definition.
+
+//Next, we can note that if a function is called using method invocation-like syntax (that is, foo.bar()), then this will be bound to the function's owner (foo), just as we'd expect, so the object's methods can do stuff to the object itself.
+
+//Finally, there's one special attribute associated with a function, prototype. This is a dictionary of values that are associated with every object that is created using the new keyword with that function; this is a good way of setting values that will be the same for every object of that "class" — for example, methods.
+
+// in the constructor function, the star is initialised with the values we provide and a starting angle of zero, and then a method is called.
 function Star(startingDistance, rotationSpeed) {
     this.angle = 0;
     this.dist = startingDistance;
@@ -229,9 +238,14 @@ function Star(startingDistance, rotationSpeed) {
     this.randomiseColors();
 }
 
+// The next step is to bind the methods to the Star function's associated prototype so that all new Stars have the same methods
+
+//draw is defined to take the parameters we passed in to it back in the main drawScene function. 
 Star.prototype.draw = function (tilt, spin, twinkle) {
+    //We start it off by pushing the current model-view matrix onto the matrix stack so that we can move around without fear of having side-effects elsewhere.
     mvPushMatrix();
 
+    //Next we rotate around the Y axis by the star's own angle, and move out by the star's distance from the centre.This puts us in the correct position to draw the star.
     // Move to the star's position
     // Update: mat4.rotate(mvMatrix, degToRad(this.angle), [0.0, 1.0, 0.0]); mat4.rotate() API has changed to mat4.rotate(out, a, rad, axis)
     // where out is the receiving matrix and a is the matrix to rotate.
@@ -240,16 +254,25 @@ Star.prototype.draw = function (tilt, spin, twinkle) {
     // where out is the receiving matrix, a is the matrix to translate, and v is the vector to translate by.
     mat4.translate(mvMatrix, mvMatrix, [this.dist, 0.0, 0.0]);
 
+    
     // Rotate back so that the star is facing the viewer
+    //These lines are required so that when we alter the tilt of the scene using the cursor keys, the stars still look right. They are drawn as a 2D texture on a square, which looks right when we're looking at it straight on, but would just look like a line if we tilted the scene so that we were viewing it from the side. For similar reasons, we also need to back out the rotation required to position the star. When you "undo" rotations like this, you need to do so in the reverse of the order you did them in the first place, so first we undo the rotation from our positioning, and then the tilt (which was done in drawScene).
     mat4.rotate(mvMatrix, mvMatrix, degToRad(-this.angle), [0.0, 1.0, 0.0]);
     mat4.rotate(mvMatrix, mvMatrix, degToRad(-tilt), [1.0, 0.0, 0.0]);
 
+    //The next lines are to draw the star
     if (twinkle) {
+
+	//the star has two colours associated with it — its normal colour, and its "twinkling colour". To make it twinkle, before we draw the star itself we draw a non-spinning star in a different colour. This means that the two stars are blended together, making a nice bright colour, but also means that rays that come out of the first star to be drawn are stationary while the ones that come out of the second star are rotating, giving a nice effect. That's our twinkling.
+	
 	// Draw a non-rotating star in the alternate "twinkling" color
 	gl.uniform3f(shaderProgram.colorUniform, this.twinkleR, this.twinkleG, this.twinkleB);
 	drawStar();
     }
 
+
+    //The star is drawn by first rotating around the Z axis by the spin that was passed in, so that it rotates around its own centre while it orbits the centre of the scene. We then push the star's colour up to the graphics card in a shader uniform, and then call a global drawStar function (which we'll come to in a moment).
+    
     // All stars spin around the Z axis at the same rate
     mat4.rotate(mvMatrix, mvMatrix, degToRad(spin), [0.0, 0.0, 1.0]);
 
@@ -257,14 +280,20 @@ Star.prototype.draw = function (tilt, spin, twinkle) {
     gl.uniform3f(shaderProgram.colorUniform, this.r, this.g, this.b);
     drawStar();
 
+    //once we've drawn the star, we just pop our model-view matrix from the stack and we're done
     mvPopMatrix();
 };
 
 
+//The next method we bind to the prototype is the one to animate the star
+// instead of just getting the scene to update as fast as it can, I've chosen to make it change at a steady pace for everyone, so people with faster computers get smoother animations and people with slower ones jerkier. Now, I think that the numbers for the angular speed at which the stars orbit the centre of the scene and at which they move towards the centre were carefully calculated by NeHe, so rather than messing around with them I decided that it was best to assume that the numbers were calibrated for 60 frames per second and then use that and the elapsedTime (which you'll remember is the time between calls to the animate function) to scale the amount we move at each animation "tick" appropriately. elapsedTime is in milliseconds, and so we want an effective frames-per-millisecond of 60 / 1000. We put this into a global variable outside the animate method, so that it won't be recalculated every time we paint a star
 var effectiveFPMS = 60 / 1000;
 Star.prototype.animate = function (elapsedTime) {
+    //now we have this number we can adjust the star's angle — that is, how far around its orbit of the centre of the scene it is
     this.angle += this.rotationSpeed * effectiveFPMS * elapsedTime;
 
+    //and we can adjust its distance from the centre, moving it out to the outside of the scene and resetting its colours to something random when it finally reaches the centre
+    
     // Decrease the distance, resetting the star to the outside of
     // the spiral if it's at the center.
     this.dist -= 0.01 * effectiveFPMS * elapsedTime;
@@ -276,6 +305,7 @@ Star.prototype.animate = function (elapsedTime) {
 };
 
 
+//The final bit of code that makes up the Star's prototype is that code we saw used in the constructor and just now in the animation code to randomise its twinkling and non-twinkling colours
 Star.prototype.randomiseColors = function () {
     // Give the star a random color for normal
     // circumstances...
