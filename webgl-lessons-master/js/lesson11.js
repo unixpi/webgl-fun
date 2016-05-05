743abe854d4e794519da018f9250ef8defcf6e48
sach <sacha.saint-leger@mail.mcgill.ca>
2016-05-05 14:42:18 -0400

    lesson 11 difficult bit digested

--- webgl-lessons-master/js/lesson11.js
+++ webgl-lessons-master/js/lesson11.js
@@ -198,15 +198,23 @@ var moonVertexPositionBuffer;
 var moonVertexNormalBuffer;
 var moonVertexTextureCoordBuffer;
 var moonVertexIndexBuffer;
-
 function initBuffers() {
+    //what are the latitude and longitude bands? In order to draw a set of triangles that are an approximation to a sphere, we need to divide it up. There are many clever ways of doing it, and one simple way based on high school geometry that (a) gets perfectly decent results and (b) I can actually understand without making my head hurt. So here’s that one :-) It’s based on one of the demos on the Khronos website, was originally developed by the WebKit team, and it works like this:
+
+//    Let’s start by defining the terminology: the lines of latitude are the ones that, on a globe, tell you how far north or how far south you are. The distance between them, as measured along the surface of the sphere, is constant. If you were to slice up a sphere from top to bottom along its lines of latitude, you’d wind up with thin lense-shaped bits for the top and the bottom, and then increasingly thick disc-like slices for the middle. (If this is hard to visualise, imagine slicing a tomato into discs for a salad, but trying to keep the same length of skin from the top to the bottom of each slice. Obviously the slices in the middle would be thicker than those at the top.)
+
+//    The lines of longitude are different; they divide the sphere into segments. If you were to slice a sphere up along its lines of longitude, it would come apart rather like an orange.
+
+    //Now, to draw our sphere, imagine that we’ve drawn the lines of latitude from top to bottom, and the lines of longitude around it. What we want to do is work out all of the points where those lines intersect, and use those as vertex positions. We can then split each square formed by two adjacent lines of longitude and two adjacent lines of latitude into two triangles, and draw them.
     var latitudeBands = 30;
     var longitudeBands = 30;
     var radius = 2;
 
+    
     var vertexPositionData = [];
     var normalData = [];
     var textureCoordData = [];
+    //loop through all of the latitudinal slices, then within that loop we run through the longitudinal segments, and we generate the normals, texture coordinates, and vertex positions for each. The only oddity to note is that our loops terminate when the index is greater than the number of longitudinal/latitudinal lines — that is, we use <= rather than < in the loop conditions. This means that for, say, 30 longitudes, we will generate 31 vertices per latitude. Because the trigonometric functions cycle, the last one will be in the same position as the first, and so this gives us an overlap so that everything joins up.
     for (var latNumber=0; latNumber <= latitudeBands; latNumber++) {
 	var theta = latNumber * Math.PI / latitudeBands;
 	var sinTheta = Math.sin(theta);
@@ -235,6 +243,10 @@ function initBuffers() {
     }
 
     var indexData = [];
+    //Now that we have the vertices, we need to stitch them together by generating a list of vertex indices that contains sequences of six values, each representing a square expressed as a pair of triangles.
+
+    //We loop through our vertices, and for each one, we store
+    //its index in first, and then count longitudeBands + 1 indices forward to find its counterpart one latitude band down — adding the one because of the extra vertices we had to add to allow for the overlap — and store that in second. We then generate two triangles, as in the diagram.
     for (var latNumber=0; latNumber < latitudeBands; latNumber++) {
 	for (var longNumber=0; longNumber < longitudeBands; longNumber++) {
 	    var first = (latNumber * (longitudeBands + 1)) + longNumber;
