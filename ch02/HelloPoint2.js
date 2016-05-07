310474f1a0a6b79be270634c405a5d1d047c0a28
sach <sacha.saint-leger@mail.mcgill.ca>
2016-05-07 14:44:27 -0400

    finished and extended helloPoint2

--- ch02/HelloPoint2.js
+++ ch02/HelloPoint2.js
@@ -1,11 +1,12 @@
-// HelloPint2.js (c) 2012 matsuda
+// HelloPoint2.js (c) 2012 matsuda
 // Vertex shader program
 var VSHADER_SOURCE = 
-  'attribute vec4 a_Position;\n' + // attribute variable
-  'void main() {\n' +
-  '  gl_Position = a_Position;\n' +
-  '  gl_PointSize = 10.0;\n' +
-  '}\n'; 
+    'attribute vec4 a_Position;\n' + // attribute variable
+    'attribute float a_Size;\n' +
+    'void main() {\n' +
+    '  gl_Position = a_Position;\n' +
+    '  gl_PointSize = a_Size;\n' +
+    '}\n'; 
 
 // Fragment shader program
 var FSHADER_SOURCE = 
@@ -30,22 +31,31 @@ function main() {
     return;
   }
 
-  // Get the storage location of a_Position
-  var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  if (a_Position < 0) {
-    console.log('Failed to get the storage location of a_Position');
-    return;
-  }
+    // Get the storage location of a_Position
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+	console.log('Failed to get the storage location of a_Position');
+	return;
+    }
 
-  // Pass vertex position to attribute variable
-  gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
-
-  // Specify the color for clearing <canvas>
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT);
+    //Get storage location of a_Size
+    var a_Size = gl.getAttribLocation(gl.program, 'a_Size');
+    if (a_Size < 0) {
+	console.log('Failed to get the storage location of a_Size');
+	return;
+    }
     
-  // Draw
-  gl.drawArrays(gl.POINTS, 0, 1);
+    // Pass vertex position to attribute variable
+    gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
+
+    // Pass vertex size to attribute variable
+    gl.vertexAttrib1f(a_Size, 10.0);
+
+    // Specify the color for clearing <canvas>
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // Clear <canvas>
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    // Draw
+    gl.drawArrays(gl.POINTS, 0, 1);
 }
