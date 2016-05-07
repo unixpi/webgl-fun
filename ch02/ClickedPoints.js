132dd0a533839c612f5dbca79a55a5fdb7c240e4
sach <sacha.saint-leger@mail.mcgill.ca>
2016-05-07 16:58:59 -0400

    added comments to ClickedPoints.js

--- ch02/ClickedPoints.js
+++ ch02/ClickedPoints.js
@@ -37,7 +37,9 @@ function main() {
     return;
   }
 
-  // Register function (event handler) to be called on a mouse press
+    // Register function (event handler) to be called on a mouse press
+    //the <canvas> supports special properties for registering event handlers
+    //for a specific user input
   canvas.onmousedown = function(ev){ click(ev, gl, canvas, a_Position); };
 
   // Specify the color for clearing <canvas>
@@ -49,24 +51,60 @@ function main() {
 
 var g_points = []; // The array for the position of a mouse press
 function click(ev, gl, canvas, a_Position) {
-  var x = ev.clientX; // x coordinate of a mouse pointer
-  var y = ev.clientY; // y coordinate of a mouse pointer
-  var rect = ev.target.getBoundingClientRect() ;
+    var x = ev.clientX; // x coordinate of a mouse pointer
+    var y = ev.clientY; // y coordinate of a mouse pointer
+    var rect = ev.target.getBoundingClientRect(); //rect.left and rect.top indicate the position of the origin of the <canvas> in the browser's client area
 
-  x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
-  y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
-  // Store the coordinates to g_points array
-  g_points.push(x); g_points.push(y);
+    //note, the x and y coordinates we have saved is the position of the mouseclick in the 'client area' in the browser, not the position in the <canvas>. Furthermore, the co-ordinate
+    //system of the <canvas> is different from that of WebGL in terms of its origin and
+    //direction of the y-axis
+    //So first we need to transform the coordinates from the browser area to the canvas,
+    //and then transform them to the WebGL co-ordinate system
+    //not in the canvas
+    //(x - rect.left) and (y-rect.top) slide the position (x,y) in the client area
+    //to the correct position on the <canvas> element
+    //to transform the <canvas> position into the WebGL coordinate system we need
+    //to know the center position of the canvas ==> (canvas.height/2, canvas.weight/2)
+    //Next, you can implement this transformation by sliding the origin of the <canvas>
+    //into the origin of the WebGL coordinate system located at the center of the <canvas>
+    
+    x = ((x - rect.left) - canvas.width/2)/(canvas.width/2);
+    y = (canvas.height/2 - (y - rect.top))/(canvas.height/2);
 
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT);
+    // Store the coordinates to g_points array
+    g_points.push(x); g_points.push(y);
+    //you may be wondering why you need to store all the positions rather than just the
+    //most recent mouse click. This is because of the way WebGL uses the color buffer.
+    //You will remember that in the WebGL system, first the drawing operation is performed
+    //to the color buffer, and then the system displays its content to the screen.
+    //After tha, the color buffer is reinitialized and its content lost. (this is the
+    //default behavior, which you'll investigate in the next section.) Therefore it is
+    //necessary to save all the positions of the clicked points in the array, so that on
+    //each mouse click, the program can draw all the previous points as the latest. For
+    //example the first point is drawn at the first mouse click. The first and second
+    //points are drawn at the second mouse click. The first, second, and third points are
+    //drawn on the third click and so on.
 
-  var len = g_points.length;
-  for(var i = 0; i < len; i += 2) {
-    // Pass the position of a point to a_Position variable
-    gl.vertexAttrib3f(a_Position, g_points[i], g_points[i+1], 0.0);
+    
+    // Clear <canvas>
+    //note: if you specify a clear color, as we have done above, then we must
+    //clear the canvas every time before we draw to ensure this clear color is
+    //used, otherwise webGL will clear anyway but will resort to the default
+    //clear color (0.0,0.0,0.0,0.0) ==> a black color, but with an alpha value of 0.0
+    //meaning it is a fully transparent black, i.e a white color.
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-    // Draw
-    gl.drawArrays(gl.POINTS, 0, 1);
-  }
+    var len = g_points.length;
+    for(var i = 0; i < len; i += 2) {
+	// Pass the position of a point to a_Position variable
+	gl.vertexAttrib3f(a_Position, g_points[i], g_points[i+1], 0.0);
+	// Draw
+	gl.drawArrays(gl.POINTS, 0, 1);
+    }
 }
+
+//Although it's a little complicated, you can see that the use of event handlers combined
+//with an attribute variable provides a flexible and generic means for user input to
+//change what WebGL draws
+
+
